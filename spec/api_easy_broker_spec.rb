a9e1feefcc96a797cdaab4455d4d22f066e6b9a3
Hector Garcia Rosales <ghectormus@gmail.com>
2024-07-22 22:27:11 -0600

    refac: changing first test to make cleaner code

--- spec/api_easy_broker_spec.rb
+++ spec/api_easy_broker_spec.rb
@@ -2,11 +2,10 @@ require 'dotenv/load'
 require 'api_easy_broker'
 require 'rspec'
 api_url=ENV['API_URL']
-api_token=ENV['API_TOKEN']
 RSpec.describe ApiEasyBroker do
-    it 'should return 200 status code' do
-      response = ApiEasyBroker.get(api_url, api_token)
-      expect(response).to eq(200)
+    it 'should return non empty value ' do
+      response = ApiEasyBroker.get(api_url)
+      expect(response).not_to be_nil
   end
 
   it 'should return a list of 50 titles' do
